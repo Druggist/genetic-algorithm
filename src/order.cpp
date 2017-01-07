3f3ac484a755918b90fe002b36c9d7fb09b5bd95
Root <pankernel@tutanota.com>
2017-01-07 22:39:00 +0100

    order

--- src/order.cpp
+++ src/order.cpp
@@ -17,39 +17,50 @@ Order::Order(vector<Task> tasks, vector<Maitenance> maitenance_v){
     machine1.init(1, tasks[0].get_ready_t());
     machine2.init(2, tasks[0].get_ready_t());
     for (unsigned int i = 0; i < tasks.size(); i++){
-        if (machine1.get_stop_t() <= tasks[i].get_ready_t()){ // when  fits perfectly
+        if (machine1.get_stop_t() >= tasks[i].get_ready_t()){ // when  fits perfectly
             while (!machine1.add(tasks[i], maitenance_v)){
                 tasks.push_back(tasks[i]);
                 tasks.erase(tasks.begin()+i);
             }
-            machine2.set_start_t(machine1.get_stop_t());
-            machine2.set_stop_t(machine1.get_stop_t());
+        if( machine2.get_stop_t() >= task[i].get_ready_t()){
             machine2.add(tasks[i], maitenance_v);
+        }else{
+            machine2.set_stop_t(task[i].get_ready_t());
+            machine2.add(tasks[i], maitenance_v);}
+
         }
         else{ // if machine1.get_stop_t() > tasks[i].get_ready_t()
             for (unsigned int j = i + 1; j < tasks.size(); j++){
-                if(machine1.get_stop_t() <= tasks[i].get_ready_t()){
+                if(machine1.get_stop_t() >= tasks[i].get_ready_t()){
                     iter_swap(tasks.begin()+i, tasks.begin()+j);
                     break;
                 }
             }
-            if (machine1.get_stop_t() <= tasks[i].get_ready_t()){
+            if (machine1.get_stop_t() >= tasks[i].get_ready_t()){
             while (!machine1.add(tasks[i], maitenance_v)){
                 tasks.push_back(tasks[i]);
                 tasks.erase(tasks.begin()+i);
             }
-                machine2.set_start_t(machine1.get_stop_t());
-                machine2.set_stop_t(machine1.get_stop_t());
+            if( machine2.get_stop_t() >= task[i].get_ready_t()){
                 machine2.add(tasks[i], maitenance_v);
+            }else{
+                machine2.set_stop_t(task[i].get_ready_t());
+                machine2.add(tasks[i], maitenance_v);}
+
+            }
             }
             else{ //move rt
             while (!machine1.add(tasks[i], maitenance_v)){
                 tasks.push_back(tasks[i]);
                 tasks.erase(tasks.begin()+i);
                 }
-                machine2.set_start_t(machine1.get_stop_t());
-                machine2.set_stop_t(machine1.get_stop_t());
+            if( machine2.get_stop_t() >= task[i].get_ready_t()){
                 machine2.add(tasks[i], maitenance_v);
+            }else{
+                machine2.set_stop_t(task[i].get_ready_t());
+                machine2.add(tasks[i], maitenance_v);}
+
+                }
             }
         }
     }
