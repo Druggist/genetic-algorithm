3e21a9f898e9955d2259559acf1b854cd2174538
Druggist <m.najdora@gmail.com>
2017-01-07 23:46:36 +0100

    Instance fixes

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 mkdir -p data
 mkdir -p bin
 
-g++ -Wall -std=c++11 src/main.cpp src/machine.cpp src/maitenance.cpp src/order.cpp src/task.cpp src/generations.cpp  -o bin/main.app
+g++ -Wall -std=c++11 src/main.cpp src/machine.cpp src/maitenance.cpp src/order.cpp src/task.cpp src/generations.cpp src/instance.cpp -o bin/main.app
--- src/instance.cpp
+++ src/instance.cpp
@@ -1,144 +1,142 @@
 #include "instance.h"
 
 /*void Instance::set_id(int i){
-  id = i;
+	id = i;
 }
 
 void Instance::set_task_number(int t){
-  tasks_number = t;
+	tasks_number = t;
 }*/
 
 void Instance::generate_instance(int quantity, int max_time, int identifier, bool verbose){
-  srand(time(NULL));
-  id = identifier;
-  tasks_number = quantity;
-  int t1_1, t1_2, rt, mt, opt;
-  Task t;
-  for (int i = 0; i < quantity; i++){
-    t1_1 = rand() % max_time+1;
-    t1_2 = rand() % max_time+1;
-    if ( i < quantity/2 ){
-      rt = 0;
-    }
-    else{
-      rt = rand() % ((max_time*(quantity/4))+1);
-    }
-    t.set_op_time(1, t1_1);
-    t.set_op_time(2, t1_2);
-    t.set_rt(rt);
-    task_v.push_back(t);
-    //debug only:
-    if (verbose == true){
-     std::cout << t1_1 << ";" << t1_2 << ";" << 1 << ";" << 2 << ";" << rt <<';'<< endl;
-    }
-  }
-  if (verbose == true){
-    std::cout << "---------------------------------------------------------------------------\n";
-  }
-  Maitenance m;
-  for (int i = 0; i < quantity/5; i++){
-    mt = rand() % (max_time /2 ) + 1;
-    opt = mt + max_time * i;
-    m.set_mt(mt);
-    m.set_opt(opt);
-    m.set_id(i);
-    if (verbose == true){
-      std::cout << i << ';' << 1 << ';' << opt << ';' << mt << ';' << endl;
-    }
-    maitenance_v.push_back(m);
-  }
+	srand(time(NULL));
+	this->id = identifier;
+	this->tasks_number = quantity;
+	int t1_1, t1_2, rt, mt, opt;
+	for (int i = 0; i < quantity; i++){
+		Task t(task_v.size());
+		t1_1 = rand() % max_time+1;
+		t1_2 = rand() % max_time+1;
+		if ( i < quantity/2 ){
+			rt = 0;
+		}else{
+			rt = rand() % ((max_time*(quantity/4))+1);
+		}
+		t.set_op_t(1, t1_1);
+		t.set_op_t(2, t1_2);
+		t.set_ready_t(rt);
+		task_v.push_back(t);
+		//debug only:
+		if (verbose == true){
+		 std::cout << t1_1 << ";" << t1_2 << ";" << 1 << ";" << 2 << ";" << rt <<';'<< endl;
+		}
+	}
+	if (verbose == true){
+		std::cout << "---------------------------------------------------------------------------\n";
+	}
+	for (int i = 0; i < quantity/5; i++){
+		Maitenance m(i);
+		mt = rand() % (max_time /2 ) + 1;
+		opt = mt + max_time * i;
+		m.set_start_t(mt);
+		m.set_duration(opt);
+		if (verbose == true){
+			std::cout << i << ';' << 1 << ';' << opt << ';' << mt << ';' << endl;
+		}
+		maitenance_v.push_back(m);
+	}
 }
 
 void Instance::dump_instance(string filename){
-  ofstream file(filename.c_str());
-  //file.open(filename.c_str());
-  if (file.good() == true){
-    file << "****" << id << "****\n";
-    file << tasks_number << endl;
-    for(vector<int>::size_type i = 0; i < task_v.size(); i++){
-      file << task_v[i].get_op_time(1) << ";" << task_v[i].get_op_time(2) << ";" << "1" <<";" << "2" << ";" << task_v[i].get_rt() << ";" << endl;
-    }
-    for (vector<int>::size_type i = 0; i < maitenance_v.size(); i++){
-      file << maitenance_v[i].get_id() <<';' << 1 << ";" << maitenance_v[i].get_opt() << ";" << maitenance_v[i].get_mt() << ";" << endl;
-    }
-  }
-  else{
-    cerr<<"Cannot open and write to instance file!\n";
-    exit(-1);
-  }
-  file.close();
-  clear_all();
+	ofstream file(filename.c_str());
+	//file.open(filename.c_str());
+	if (file.good() == true){
+		file << "****" << id << "****\n";
+		file << tasks_number << endl;
+		for(vector<int>::size_type i = 0; i < task_v.size(); i++){
+			file << task_v[i].get_op_t(1) << ";" << task_v[i].get_op_t(2) << ";" << "1" <<";" << "2" << ";" << task_v[i].get_ready_t() << ";" << endl;
+		}
+		for (vector<int>::size_type i = 0; i < maitenance_v.size(); i++){
+			file << maitenance_v[i].get_id() <<';' << 1 << ";" << maitenance_v[i].get_duration() << ";" << maitenance_v[i].get_start_t() << ";" << endl;
+		}
+	}
+	else{
+		cerr<<"Cannot open and write to instance file!\n";
+		exit(-1);
+	}
+	file.close();
+	clear_all();
 }
 
 void Instance::read_instance(string filename){
-  string s;
-  int quantity, tmp;
-  Task t;
-  Maitenance m;
-  fstream file;
-  file.open(filename.c_str());
-  if(file.good() == true){
-    getline(file, s); //ignoring one line
-    getline(file, s);
-      quantity = atoi(s.c_str());
-      std::cout << quantity << endl;
-      for (int i = 0; i < quantity; i++){
-        getline(file, s, ';');
-        tmp = atoi(s.c_str());
-        t.set_op_time(1, tmp);
-        getline(file, s, ';');
-        tmp = atoi(s.c_str());
-        t.set_op_time(2,tmp);
-        getline(file, s, ';'); //ignoring two lines
-        getline(file, s, ';');
-        getline(file, s, ';');
-        tmp = atoi(s.c_str());
-        t.set_rt(tmp);
-        task_v.push_back(t);
-        //std::cout << t.get_op_time(1) << ";" << t.get_op_time(2) << ";" << "1" <<";" << "2" << ";" << t.get_rt() << ";" << endl;
-      }
+	string s;
+	int quantity, tmp;
+	fstream file;
 
-    for(int i = 0; i < quantity/5; i++){
-      getline(file, s, ';');
-      tmp = atoi(s.c_str());
-      m.set_id(tmp);
-      getline(file, s, ';'); // ignoring machine number
-      getline(file, s, ';');
-      tmp = atoi(s.c_str());
-      m.set_opt(tmp);
-      getline(file, s, ';');
-      tmp = atoi(s.c_str());
-      m.set_mt(tmp);
-      maitenance_v.push_back(m);
-      //std::cout <<  m.get_id() << ";" << m.get_opt() << ";" << m.get_mt() << ";" << endl;
-    }
+	file.open(filename.c_str());
+	if(file.good() == true){
+		getline(file, s); //ignoring one line
+		getline(file, s);
+			quantity = atoi(s.c_str());
+			std::cout << quantity << endl;
+			for (int i = 0; i < quantity; i++){
+				Task t(i);
+				getline(file, s, ';');
+				tmp = atoi(s.c_str());
+				t.set_op_t(1, tmp);
+				getline(file, s, ';');
+				tmp = atoi(s.c_str());
+				t.set_op_t(2,tmp);
+				getline(file, s, ';'); //ignoring two lines
+				getline(file, s, ';');
+				getline(file, s, ';');
+				tmp = atoi(s.c_str());
+				t.set_ready_t(tmp);
+				task_v.push_back(t);
+				//std::cout << t.get_op_time(1) << ";" << t.get_op_time(2) << ";" << "1" <<";" << "2" << ";" << t.get_rt() << ";" << endl;
+			}
+		
+		for(int i = 0; i < quantity/5; i++){
+			getline(file, s, ';');
+			tmp = atoi(s.c_str());
+			Maitenance m(tmp);
+			getline(file, s, ';'); // ignoring machine number
+			getline(file, s, ';');
+			tmp = atoi(s.c_str());
+			m.set_duration(tmp);
+			getline(file, s, ';');
+			tmp = atoi(s.c_str());
+			m.set_start_t(tmp);
+			maitenance_v.push_back(m);
+			//std::cout <<  m.get_id() << ";" << m.get_opt() << ";" << m.get_mt() << ";" << endl;
+		}
 
-  }
-  else{
-    cerr <<"Cannot open and read instance file!\n";
-    exit(-1);
-  }
-  file.close();
+	}
+	else{
+		cerr <<"Cannot open and read instance file!\n";
+		exit(-1);
+	}
+	file.close();
 }
 
 void Instance::clear_task_v(){
-  task_v.clear();
+	task_v.clear();
 }
 
 void Instance::clear_maitenance_v(){
-  maitenance_v.clear();
+	maitenance_v.clear();
 }
 
 void Instance::clear_all(){
-  clear_task_v();
-  clear_maitenance_v();
+	clear_task_v();
+	clear_maitenance_v();
 }
 
 vector<Task> Instance::get_tasks(){
-  return this->task_v;
+	return this->task_v;
 }
 
 vector<Maitenance> Instance::get_maitenances(){
-  return this->maitenance_v;
+	return this->maitenance_v;
 }
 
