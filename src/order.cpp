53d6dac213ea29d4f45d4cf96331bdb837129119
Druggist <m.najdora@gmail.com>
2017-01-08 02:58:42 +0100

    Indentations in order.cpp

--- src/order.cpp
+++ src/order.cpp
@@ -2,20 +2,23 @@
 
 void Order::init(vector<Task> tasks, vector<Maitenance> maitenance_v){
 	int machine1_ready_time = 10000, first_job_rt_pos = 0, iter;
-	if (tasks[0].get_ready_t() == 0) {machine1_ready_time = 0;}
+
+	if (tasks[0].get_ready_t() == 0) machine1_ready_time = 0;
 	else{
-		   //finding smallest ready_time
-			for (unsigned int i = 0; i < tasks.size(); i++){
-				if (tasks[i].get_ready_t() < machine1_ready_time) {
+		//finding smallest ready_time
+		for (unsigned int i = 0; i < tasks.size(); i++){
+			if (tasks[i].get_ready_t() < machine1_ready_time){
 				machine1_ready_time = tasks[i].get_ready_t();
 				first_job_rt_pos = i;
 			}
-	  }
-	//swapping jobs with first
+		}
+		//swapping jobs with first
 		iter_swap(tasks.begin()+0, tasks.begin()+first_job_rt_pos);
 	}
+
 	machine1.init(1, tasks[0].get_ready_t());
 	machine2.init(2, tasks[0].get_ready_t());
+
 	for (unsigned int i = 0; i < tasks.size(); i++){
 		iter = i;
 
@@ -25,20 +28,18 @@ void Order::init(vector<Task> tasks, vector<Maitenance> maitenance_v){
 				tasks.push_back(tasks[i]);
 				tasks.erase(tasks.begin()+i);
 				iter++;
-				if (iter == tasks.size())
-				{
+				if (iter == tasks.size()){
 					iter = i;
 					machine1.set_stop_t(ma);
 				}
 				ma=machine1.add(tasks[i], maitenance_v);
 			}
-			if( machine2.get_stop_t() >= machine1.get_stop_t()){
+			if( machine2.get_stop_t() >= machine1.get_stop_t()) machine2.add(tasks[i], maitenance_v);
+			else{
+				machine2.set_stop_t(machine1.get_stop_t());
 				machine2.add(tasks[i], maitenance_v);
-		}else{
-			machine2.set_stop_t(machine1.get_stop_t());
-			machine2.add(tasks[i], maitenance_v);}
-		}
-		else{ // if machine1.get_stop_t() > tasks[i].get_ready_t()
+			}
+		}else{ // if machine1.get_stop_t() > tasks[i].get_ready_t()
 			for (unsigned int j = i; j < tasks.size(); j++){
 				if(machine1.get_stop_t() >= tasks[j].get_ready_t()){
 					iter_swap(tasks.begin()+i, tasks.begin()+j);
@@ -51,7 +52,6 @@ void Order::init(vector<Task> tasks, vector<Maitenance> maitenance_v){
 			}
 			i--;
 		}
-
 	}
 }
 
