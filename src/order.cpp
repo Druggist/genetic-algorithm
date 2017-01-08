18edad66cbb2a655151ca9293fb059b9976fed64
Root <pankernel@tutanota.com>
2017-01-08 01:24:09 +0100

    infinite loop kill

--- src/order.cpp
+++ src/order.cpp
@@ -1,7 +1,7 @@
 #include "order.h"
 
 void Order::init(vector<Task> tasks, vector<Maitenance> maitenance_v){
-	int machine1_ready_time = 10000, first_job_rt_pos = 0;
+	int machine1_ready_time = 10000, first_job_rt_pos = 0, iter;
 	if (tasks[0].get_ready_t() == 0) {machine1_ready_time = 0;}
 	else{
 		   //finding smallest ready_time
@@ -17,10 +17,18 @@ void Order::init(vector<Task> tasks, vector<Maitenance> maitenance_v){
 	machine1.init(1, tasks[0].get_ready_t());
 	machine2.init(2, tasks[0].get_ready_t());
 	for (unsigned int i = 0; i < tasks.size(); i++){
+		iter = i;
 		if (machine1.get_stop_t() >= tasks[i].get_ready_t()){ // when  fits perfectly
+
 			while (!machine1.add(tasks[i], maitenance_v)){
 				tasks.push_back(tasks[i]);
 				tasks.erase(tasks.begin()+i);
+                iter++;
+                if (iter == tasks.size())
+                {
+                    iter = i;
+                    break;
+                }
 			}
 		if( machine2.get_stop_t() >= tasks[i].get_ready_t()){
 			machine2.add(tasks[i], maitenance_v);
@@ -40,7 +48,13 @@ void Order::init(vector<Task> tasks, vector<Maitenance> maitenance_v){
 				while (!machine1.add(tasks[i], maitenance_v)){
 					tasks.push_back(tasks[i]);
 					tasks.erase(tasks.begin()+i);
-				}
+                    iter++;
+                    if (iter == tasks.size())
+                    {
+                        iter = i;
+                        break;
+                    }
+                }
 				if( machine2.get_stop_t() >= tasks[i].get_ready_t()){
 					machine2.add(tasks[i], maitenance_v);
 				}else{
@@ -50,6 +64,12 @@ void Order::init(vector<Task> tasks, vector<Maitenance> maitenance_v){
 				while (!machine1.add(tasks[i], maitenance_v)){
 					tasks.push_back(tasks[i]);
 					tasks.erase(tasks.begin()+i);
+                    iter++;
+                    if (iter == tasks.size())
+                    {
+                        iter = i;
+                        break;
+                    }
 					}
 				if( machine2.get_stop_t() >= tasks[i].get_ready_t()){
 					machine2.add(tasks[i], maitenance_v);
