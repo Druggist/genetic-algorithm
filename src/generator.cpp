95b9ef35db78db85da44d75d14c3cb2f69392fdf
rav <programista15k@tutanota.com>
2017-01-10 19:19:08 +0100

    generator

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,12 +1,19 @@
 #include "generator.h"
 
 int main(int argc, char * argv[]){
-  
-  if (argc != 6){
-    std::cout << "Usage:\n" << argv[0] << "<number_of_instances> <number_of_tasks> <max_time> " << endl;
+   int quantity, max_time, n, m;
+   string dir = "";
+  if (argc < 6){
+    std::cout << "Usage:\n" << argv[0] << "<number_of_instances> <number_of_tasks> <max_time> <start_count> <output_directory> " << endl;
     exit(0);
   }
-  int quantity, max_time, n;
+  if (argc == 5) {
+    m = atoi(argv[4]);
+    dir = "../data";
+  }else{
+    m = atoi(argv[4]);
+    dir = argv[5];
+  }
   string filename = "";
   max_time = atoi(argv[3]);
   quantity = atoi(argv[1]);
@@ -14,7 +21,7 @@ int main(int argc, char * argv[]){
   for (int i = 0; i < quantity; i++){
   	Instance I;
   	I.generate_instance(n, max_time ,i, false);
-  	filename = "../data/file_" + std::to_string(i) + ".txt";
+  	filename = dir + "/file_" + std::to_string(i) + ".txt";
   	I.dump_instance(filename);
   }
   //Instance I;
