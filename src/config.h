5a9200332e3cb72bab49d2687e4ccca4b149c403
Druggist <m.najdora@gmail.com>
2017-01-10 19:39:14 +0100

    Initial tuning

--- src/config.h
+++ src/config.h
@@ -5,9 +5,7 @@ static unsigned int _POPULATION_SIZE = 100;
 static unsigned short _REMOVE_PCT = 50;
 static unsigned short _REMOVE_FAIL_PCT = 3;
 static unsigned short _MIN_RESECTION_PCT = 10;
-static unsigned short _MAX_RESECTION_PCT = 90;
+static unsigned short _MAX_RESECTION_PCT = 100 - _MIN_RESECTION_PCT;
 static unsigned short _MUTATION_CHANCE_PCT = 10;
-static unsigned short _EXEC_TIME_WEIGHT = 1;
-static unsigned short _IMPROVE_WEIGHT = 1;
 static unsigned int _EXEC_TIME_SECS = 5 * 60;
 #endif // CONFIG_H
