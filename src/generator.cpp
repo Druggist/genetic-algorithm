1d4178be58912d5466345240972a60dd667bd881
rav <programista15k@tutanota.com>
2017-01-10 19:23:28 +0100

    gen vol2

--- src/generator.cpp
+++ src/generator.cpp
@@ -8,11 +8,11 @@ int main(int argc, char * argv[]){
     exit(0);
   }
   if (argc == 5) {
-    m = atoi(argv[4]);
+    m = atoi(argv[5]);
     dir = "../data";
   }else{
-    m = atoi(argv[4]);
-    dir = argv[5];
+    m = atoi(argv[5]);
+    dir = argv[6];
   }
   string filename = "";
   max_time = atoi(argv[3]);
@@ -21,8 +21,9 @@ int main(int argc, char * argv[]){
   for (int i = 0; i < quantity; i++){
   	Instance I;
   	I.generate_instance(n, max_time ,i, false);
-  	filename = dir + "/file_" + std::to_string(i) + ".txt";
+  	filename = dir + "/file_" + std::to_string(m) + ".txt";
   	I.dump_instance(filename);
+    m++;
   }
   //Instance I;
   //I.read_instance("file_0.txt");
