2152a9e99d13821135534959ab37ef1d28225f35
Druggist <m.najdora@gmail.com>
2017-01-10 22:34:17 +0100

    Resection tuning

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 static unsigned int _POPULATION_SIZE = 100;
 static unsigned short _REMOVE_PCT = 50;
 static unsigned short _REMOVE_FAIL_PCT = 6;
-static unsigned short _MIN_RESECTION_PCT = 10;
+static unsigned short _MIN_RESECTION_PCT = 20;
 static unsigned short _MAX_RESECTION_PCT = 100 - _MIN_RESECTION_PCT;
 static unsigned short _MUTATION_CHANCE_PCT = 10;
 static unsigned int _EXEC_TIME_SECS = 5 * 60;
