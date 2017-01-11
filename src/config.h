c8b5e0312c5f4db00b7db302a868060c30aeafae
Druggist <m.najdora@gmail.com>
2017-01-11 21:56:13 +0100

    Tuning

--- src/config.h
+++ src/config.h
@@ -1,11 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-static unsigned int _POPULATION_SIZE = 500;
+static unsigned int _POPULATION_SIZE = 200;
 static unsigned short _REMOVE_PCT = 50;
-static unsigned short _REMOVE_FAIL_PCT = 6;
-static unsigned short _MIN_RESECTION_PCT = 20;
+static unsigned short _REMOVE_FAIL_PCT = 7;
+static unsigned short _MIN_RESECTION_PCT = 30;
 static unsigned short _MAX_RESECTION_PCT = 100 - _MIN_RESECTION_PCT;
-static unsigned short _MUTATION_CHANCE_PCT = 10;
+static unsigned short _MUTATION_CHANCE_PCT = 15;
 static unsigned int _EXEC_TIME_SECS = 1 * 60;
 #endif // CONFIG_H
