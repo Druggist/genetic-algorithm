82d50704d8887e82fae2beb20fb53894b2059b35
Druggist <m.najdora@gmail.com>
2017-01-12 02:23:38 +0100

    Chart1 data fix

--- chart1_data.sh
+++ chart1_data.sh
@@ -5,7 +5,7 @@ do
 	touch data/chart1/out/0$i/data.tx
 	for f in data/chart1/out/0$i/*.txt
 	do
-		cat $f | head -n 2 | tail -n 1 >> data/chart1/out/0$i/data.tx
+		cat $f | head -n 2 | tail -n 1 | cut -c1-4 >> data/chart1/out/0$i/data.tx
 	done
 	mv data/chart1/out/0$i/data.tx data/chart1/out/0$i/data.txt
 done
